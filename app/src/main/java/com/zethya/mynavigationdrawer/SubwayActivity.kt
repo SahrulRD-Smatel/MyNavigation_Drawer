b4c801af9fcd60f9266e3423e3c1c800204a4efa
Sahrul Ramadhani <sahrul.r.dhani@gmail.com>
2020-11-27 17:01:53 +0700

    initial

---
+++ app/src/main/java/com/zethya/mynavigationdrawer/SubwayActivity.kt
@@ -0,0 +1,11 @@
+package com.zethya.mynavigationdrawer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SubwayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_subway)
+    }
+}
